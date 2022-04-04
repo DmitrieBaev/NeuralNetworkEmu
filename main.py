fcdc52dd6bb51c649a9ff9c73f0c345290c7119d
Quotoms <azo.cw@yandex.ru>
2022-04-04 16:53:27 +0300

    Исправлены промежуточные значения. Завершен эмулятор нейронной сети.

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ from neural import handler
 
 def main():
     try:
+        # N = int(input('Введите количество нейронов: '))
         # a = int(input('Введите начало операционного отрезка: '))
         # b = int(input('Введите конец операционного отрезка: '))
-        # N = int(input('Введите количество нейронов: '))
         # pointX = float(input('Введите координату X искомой точки: '))
-        # pointY = float(input('Введите координату Y искомой точки: '))
 
-        handler.configure(a=0, b=10, N=10)
-        handler.calculate(point=8.0)
+        handler.N = 5  # Количество нейронов
+        handler.configure(a=0, b=10)
+        result_parser(handler.calculate(point=3.9))
     except ValueError:
         print('[X] Некорректные данные!')
         main()
+
+
+def result_parser(result: list):
+    """
+    Обрабатывает результат, полученный от нейронной сети и выводит в консоль нужную информацию
+    """
+
+    for i in result:
+        if i != 0:
+            print(f'{i:.2f}')
+            break
 
 
 if __name__ == '__main__':
--- neural/handler.py
+++ neural/handler.py
@@ -5,9 +5,10 @@ A = []  # -----------------------
 B = []  # Необходимые данные для
 C = []  # работы нейронной сети
 D = []  # -----------------------
+N = 0   # Количество нейронов
 
 
-def configure(a: float, b: float, N: int):
+def configure(a: float, b: float):
     """
     Инициализация значений, необходимых для нейронной сети
 
@@ -26,9 +27,11 @@ def configure(a: float, b: float, N: int):
     # Подготовка значений для вычисления нейронной сети
     for i in range(N):
         A.append((X[i] ** 2 - X[i + 1] ** 2) / (X[i] - X[i + 1]))
-        B.append(X[i] ** 2 + X[i + 1] ** 2)
+        B.append(-X[i]*X[i + 1])
         C.append(1 / (X[i + 1] - X[i]))
         D.append(-(X[i] / (X[i + 1] - X[i])))
+
+    print(f'X:{X}\nA:{A}\nB:{B}\nC:{C}\nD:{D}')
 
 
 def calculate(point: float):
@@ -36,13 +39,36 @@ def calculate(point: float):
     Запуск нейронной сети для обработки функции
 
     :param point: 0X координата точки, которую нужно найти
-
-    :return:
+    :return: Список отрезков, в котором ненулевое значение - подходящая точка на оси 0Y
     """
+    def layer_1_solution():
+        """ Обработка 1-го слоя нейронной сети """
+        y = []
+        for i in range(N):
+            y.append(point * A[i] + B[i])
+        return y
 
-    pass
+    def layer_2_solution():
+        """ Обработка 2-го слоя нейронной сети """
+        y = []
+        for i in range(N):
+            y.append(1 if 0.0 <= (point * C[i] + D[i]) < 1.0 else 0)
+        return y
+
+    def layer_3_solution(l1: list, l2: list):
+        """ Обработка 3-го слоя нейронной сети """
+        y = []
+        for i in range(N):
+            y.append(l1[i] * l2[i])
+        return y
+
+    layer1 = layer_1_solution()
+    layer2 = layer_2_solution()
+
+    return layer_3_solution(layer1, layer2)
 
 
 if __name__ == "__main__":
-    configure(float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3]))
+    N = sys.argv[1]
+    configure(float(sys.argv[2]), float(sys.argv[3]))
     calculate(float(sys.argv[4]))
